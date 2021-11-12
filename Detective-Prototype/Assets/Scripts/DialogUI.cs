7c0e9a8107d736c39bb2a8866c7032d76363f7be
rbeard7 <84631913+rbeard7@users.noreply.github.com>
2021-11-12 11:50:01 -0800

    Dialog box - functional

---
+++ Detective-Prototype/Assets/Scripts/DialogUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using TMPro;
+
+public class DialogUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textLabel;
+
+    private void Start()
+    {
+        textLabel.text = "Hello!\nThis is my second line.";
+    }
+}
--- Detective-Prototype/Assets/Scripts/SelectionManager.cs
+++ Detective-Prototype/Assets/Scripts/SelectionManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+
 
 public class SelectionManager : MonoBehaviour
 {
@@ -14,7 +16,17 @@ public class SelectionManager : MonoBehaviour
 
     private Transform _selection;
 
-    public bool gamePaused = false;
+    public bool gamePaused = false;             // public due to ThirdPersonMovement using this
+    public GameObject dialogBox;                // Assigned in editor for enable/disable
+    public TMP_Text textLabel;                  // Assigned in editor for assigning text
+
+
+    private void Start()
+    {
+        dialogBox.SetActive(false);
+    }
+
+
     void Update()
     {
         
@@ -30,10 +42,12 @@ public class SelectionManager : MonoBehaviour
                 if (gamePaused == false)
                 {
                     gamePaused = true;
+                    
                 }
                 else
                 {
                     gamePaused = false;
+                    
                 }
             }
         }
@@ -51,17 +65,37 @@ public class SelectionManager : MonoBehaviour
                 {
                     selectionRenderer.material = selectedMaterial;
                 }
+
+                // Assign a value to the text box based upon object
+
+                if (selection.name == "Sphere")
+                {
+                    //Debug.Log("This is the sphere");
+                    textLabel.text = "This is the sphere.";
+                } else if (selection.name == "Capsule")
+                {
+                    //Debug.Log("This is the capsule");
+                    textLabel.text = "This is the capsule.";
+                } else if (selection.name == "Cylinder")
+                {
+                    //Debug.Log("This is the cylinder");
+                    textLabel.text = "This is the cylinder.";
+                }
                 _selection = selection;
             }
         }
 
+
+        // Added to pause the game while dialog box is present on-screen
         if (gamePaused == true)
         {
             Time.timeScale = 0.0f;
+            dialogBox.SetActive(true);
         }
         else
         {
             Time.timeScale = 1.0f;
+            dialogBox.SetActive(false);
         }
     }
 }
